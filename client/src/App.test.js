b4e1f9dc61d4a2d62c79895a42db8c40746a50c7
Josh <lexar@gmail.com>
2020-12-03 15:31:28 -0800

    new

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,12 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-<<<<<<< HEAD
-test('renders learn react link', () => {});
-=======
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
->>>>>>> f9d9395446baf41988cc15763956ddb20e6b34d5
